3a962e63e0f201fd9ff7c77d0bea62aeb9cd68d1
adam <bcuz@users.noreply.github.com>
2019-02-18 22:23:43 -0500

    nav system ok for now

--- js/index.js
+++ js/index.js
@@ -1,28 +1,17 @@
-// document.querySelector('header img').addEventListener('click', (e) => {
-//   e.stopPropagation()
-
-//   if (!document.body.classList.contains('overlay')) {
-//     document.body.classList.add("overlay")
-//   } 
-
-//   if (document.body.classList.contains('remove')) {
-//     document.body.classList.remove("remove")
-//   }
-  
-// })
-
-// // https://stackoverflow.com/questions/7478336/only-detect-click-event-on-pseudo-element
-// document.querySelector('body').addEventListener('click', (e) => {
-//   if (document.body.classList.contains('overlay')) {
-//     document.body.classList.add("remove")    
-//   } 
-// })
-
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
+  document.querySelector("header").style.zIndex = "0";
+  // document.querySelector("header").style.position = "static";
+
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
+// document.querySelector('.closeNav').addEventListener('click', (e) => {
+  //     document.getElementById("myNav").style.width = "0%";
+  //   })
+  /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
+  document.querySelector("header").style.zIndex = "1";
+  // document.querySelector("header").style.position = "fixed";
+
 }
