b84521fd84554aa35a20318271726f99ff042556
adam <bcuz@users.noreply.github.com>
2019-02-21 22:04:27 -0500

    mostly text replacing

--- js/index.js
+++ js/index.js
@@ -74,13 +74,17 @@ function openNav() {
 
 }
 
-// document.querySelector('.closeNav').addEventListener('click', (e) => {
-  //     document.getElementById("myNav").style.width = "0%";
-  //   })
   /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
   document.querySelector("header").style.zIndex = "1";
   document.querySelector("header").style.position = "fixed";
-
 }
+
+document.addEventListener('keydown', event => {
+  if (event.keyCode == 69) {
+    closeNav()
+  } else if (event.keyCode == 79) {
+    openNav()
+  }
+})
